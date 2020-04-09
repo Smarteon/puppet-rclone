9f1fc6b1e17594966c26f4e1bd89c4bce0f0de0d
Jiří Mikulášek <jiri.mikulasek@gooddata.com>
2020-04-09 22:34:22 +0200

    Adjust README and generate REFERENCE

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,9 @@
-# @summary A short summary of the purpose of this class
+# @summary Download and install Rclone
 #
-# A description of what this class does
+# Install rclone binary and man page
+#
+# @param [String] version
+#   installed version
 #
 # @example
 #   include rclone
