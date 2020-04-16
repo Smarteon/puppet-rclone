6dc898a6553964184e486fa2c94e3ed5fdb28429
Jiří Mikulášek <jiri.mikulasek@gooddata.com>
2020-04-16 08:52:12 +0200

    Fix last version to strip also newlines

--- lib/puppet/functions/rclone/last_version.rb
+++ lib/puppet/functions/rclone/last_version.rb
@@ -11,7 +11,7 @@ Puppet::Functions.create_function(:'rclone::last_version') do
     uri = URI('https://downloads.rclone.org/version.txt')
     Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
       response = http.request Net::HTTP::Get.new(uri)
-      response.body.gsub(%r{.*rclone v(\d+\.\d+\.\d+).*}, '\1')
+      response.body.gsub(%r{.*rclone v(\d+\.\d+\.\d+).*}m, '\1')
     end
   end
 end
--- spec/functions/last_version_spec.rb
+++ spec/functions/last_version_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 
 describe 'rclone::last_version' do
   before(:each) do
-    stub_request(:get, 'https://downloads.rclone.org/version.txt').to_return(body: 'rclone v1000.0.1')
+    stub_request(:get, 'https://downloads.rclone.org/version.txt').to_return(body: "rclone v1000.0.1\n")
   end
 
   it { is_expected.to run.and_return('1000.0.1') }
